3bcb98dd047a0008f4ec72962bde56c7feeb1368
AlaaAlkhaled <alaa.alkhaled7@gmail.com>
2023-04-04 23:30:58 +0200

    Starting on JS
    Starting on JS by adding creating the main elements and working on the functions.

--- todolist.js
+++ todolist.js
@@ -1,1 +1,25 @@
 
+let input = document.getElementById("taskInput");
+let submit = document.getElementById("addTaskButton");
+let taskElements = document.getElementById("taskList");
+
+let arrayOfTheTasks = [];
+
+// add Task
+submit.onclick = function (){
+    if (input.value !== ""){
+        // adding tasks to array 
+        addTaskToArray(input.value); 
+        input.value = "";
+    }
+};
+
+function addTaskToArray (taskText) {
+    //data
+    const task = {
+        tittle: taskText,
+        completed: false,
+    };
+    arrayOfTheTasks.push(task);
+    //console.log(task);
+};
